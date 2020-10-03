74b5655c6c3d01bf9a90d3fe7da8f1fbe113a8d1
Edvinas01 <edvinas108@gmail.com>
2020-10-03 13:03:33 +0200

    Add sample scripts, scenes, prefabs, assets and assembly def

---
+++ Samples/GameEvents/Scripts/Spawner.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Spawner : MonoBehaviour
+{
+    [Min(0)]
+    [SerializeField]
+    private int spawnedLimit = 10;
+
+    [SerializeField]
+    private GameObject spawnPrefab = default;
+
+    private new BoxCollider collider;
+    private int spawned;
+
+    private void Awake()
+    {
+        collider = GetComponent<BoxCollider>();
+    }
+
+    private void Update()
+    {
+        if (IsSpawn()) Spawn();
+    }
+
+    private bool IsSpawn()
+    {
+        return spawned < spawnedLimit;
+    }
+
+    private void Spawn()
+    {
+        Instantiate(spawnPrefab, GetSpawnPosition(), GetSpawnRotation(), transform);
+        spawned++;
+    }
+
+    private Vector3 GetSpawnPosition()
+    {
+        var colliderBounds = collider.bounds;
+        var position = new Vector3(
+            Random.Range(colliderBounds.min.x, colliderBounds.max.x),
+            Random.Range(colliderBounds.min.y, colliderBounds.max.y),
+            Random.Range(colliderBounds.min.z, colliderBounds.max.z)
+        );
+
+        return colliderBounds.ClosestPoint(position);
+    }
+
+    private static Quaternion GetSpawnRotation()
+    {
+        var angles = new Vector3(
+            GetRandomAngle(),
+            GetRandomAngle(),
+            GetRandomAngle()
+        );
+
+        return Quaternion.Euler(angles);
+    }
+
+    private static float GetRandomAngle()
+    {
+        return Random.Range(0f, 360f);
+    }
+
+    public void DecreaseSpawned()
+    {
+        spawned--;
+    }
+}
