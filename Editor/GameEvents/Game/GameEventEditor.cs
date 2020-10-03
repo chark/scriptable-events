a88d8b25c855e798d9edf9a84a246957f3e28e89
Edvinas01 <edvinas108@gmail.com>
2020-10-03 12:55:55 +0200

    Add game event editors which would allow to trigger them via the inspector

---
+++ Editor/GameEvents/Game/GameEventEditor.cs
@@ -0,0 +1,40 @@
+﻿using GameEvents.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace GameEvents.Game
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(GameEvent))]
+    public class GameEventEditor : Editor
+    {
+        #region Pirvate Fields
+
+        private const int GroupSpacingPixels = 8;
+
+        #endregion
+
+        #region Public Methods
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var gameEvent = target as GameEvent;
+            if (gameEvent == null) return;
+
+            GUI.enabled = Application.isPlaying;
+            GUILayout.Space(GroupSpacingPixels);
+
+            DrawRaise(gameEvent);
+        }
+
+        private static void DrawRaise(IGameEvent gameEvent)
+        {
+            GUILayout.Label("Raise event (play mode only)");
+            if (GUILayout.Button("Raise")) gameEvent.RaiseGameEvent();
+        }
+
+        #endregion
+    }
+}
---
+++ Editor/GameEvents/Vector3/Vector3GameEventEditor.cs
@@ -0,0 +1,16 @@
+﻿using GameEvents.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace GameEvents.Vector3
+{
+    [CustomEditor(typeof(Vector3GameEvent))]
+    public class Vector3GameEventEditor
+        : ArgumentGameEventEditor<Vector3GameEvent, UnityEngine.Vector3>
+    {
+        protected override UnityEngine.Vector3 DrawArgumentField(UnityEngine.Vector3 value)
+        {
+            return EditorGUILayout.Vector3Field(GUIContent.none, value);
+        }
+    }
+}
