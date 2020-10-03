17877ea5c7d578fd1f2ff4d7c777c58eca04c469
Edvinas01 <edvinas108@gmail.com>
2020-10-03 12:55:55 +0200

    Add game event editors which would allow to trigger them via the inspector

---
+++ Assets/Editor/GameEvents/Generic/ArgumentGameEventEditor.cs
@@ -0,0 +1,57 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace GameEvents.Generic
+{
+    [CanEditMultipleObjects]
+    public abstract class ArgumentGameEventEditor<TGameEvent, TArgument>
+        : Editor where TGameEvent : class, IArgumentGameEvent<TArgument>
+    {
+        #region Protected Methods
+
+        /// <returns>
+        ///     Value that is entered in the argument field.
+        /// </returns>
+        protected virtual TArgument DrawArgumentField(TArgument value)
+        {
+            return value;
+        }
+
+        #endregion
+
+        #region Pirvate Fields
+
+        private const int GroupSpacingPixels = 8;
+
+        private TArgument argumentValue = default;
+
+        #endregion
+
+        #region Public Methods
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (!(target is TGameEvent gameEvent)) return;
+
+            GUI.enabled = Application.isPlaying;
+            GUILayout.Space(GroupSpacingPixels);
+
+            DrawRaise(gameEvent);
+        }
+
+        private void DrawRaise(TGameEvent gameEvent)
+        {
+            GUILayout.Label("Raise event (play mode only)");
+            GUILayout.BeginHorizontal();
+
+            argumentValue = DrawArgumentField(argumentValue);
+            if (GUILayout.Button("Raise")) gameEvent.RaiseGameEvent(argumentValue);
+
+            GUILayout.EndHorizontal();
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Editor/GameEvents/Vector2/Vector2GameEventEditor.cs
@@ -0,0 +1,16 @@
+﻿using GameEvents.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace GameEvents.Vector2
+{
+    [CustomEditor(typeof(Vector3GameEvent))]
+    public class Vector2GameEventEditor
+        : ArgumentGameEventEditor<Vector3GameEvent, UnityEngine.Vector2>
+    {
+        protected override UnityEngine.Vector2 DrawArgumentField(UnityEngine.Vector2 value)
+        {
+            return EditorGUILayout.Vector2Field(GUIContent.none, value);
+        }
+    }
+}
