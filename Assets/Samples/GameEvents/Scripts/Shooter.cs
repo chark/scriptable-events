108898899e126a46e7c995b90064763e97698bdd
Edvinas01 <edvinas108@gmail.com>
2020-10-03 13:03:33 +0200

    Add sample scripts, scenes, prefabs, assets and assembly def

---
+++ Assets/Samples/GameEvents/Scripts/Shooter.cs
@@ -0,0 +1,36 @@
+﻿using GameEvents.Transform;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class Shooter : MonoBehaviour
+{
+    [SerializeField]
+    private TransformGameEvent shotGameEvent = default;
+
+    [SerializeField]
+    private string shootButton = "Fire1";
+
+    private new Camera camera;
+
+    private void Awake()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (IsShoot()) Shoot();
+    }
+
+    private bool IsShoot()
+    {
+        return Input.GetButtonDown(shootButton);
+    }
+
+    private void Shoot()
+    {
+        var ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hit)) shotGameEvent.RaiseGameEvent(hit.transform);
+    }
+}
---
+++ Assets/Samples/GameEvents/Scripts/ShotHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ShotHandler : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent onShot = default;
+
+    public void HandleShot(Transform shot)
+    {
+        Destroy(shot.gameObject);
+        onShot.Invoke();
+    }
+}
