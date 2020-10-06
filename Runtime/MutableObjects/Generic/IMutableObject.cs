65db3a8ee52b3ed660edbb365c807c3de1cf570f
Edvinas Danevičius <edvinas108@gmail.com>
2020-10-06 16:40:48 +0200

    Automate mutable values (#12)
    * Enable persisting of mutable objects throughout scenes, replace extensions with Unity hooks
    
    * Update changelog and docs

--- Runtime/MutableObjects/Generic/IMutableObject.cs
+++ Runtime/MutableObjects/Generic/IMutableObject.cs
@@ -3,6 +3,11 @@
     public interface IMutableObject
     {
         /// <summary>
+        ///     Is this mutable object persisting throughout the scenes and should not be reset.
+        /// </summary>
+        bool Persisting { get; }
+
+        /// <summary>
         ///     Reset values on this mutable object to their original ones.
         /// </summary>
         void ResetValues();
--- Runtime/MutableObjects/Generic/MutableObject.cs
+++ Runtime/MutableObjects/Generic/MutableObject.cs
@@ -4,6 +4,12 @@ namespace MutableObjects.Generic
 {
     public abstract class MutableObject : ScriptableObject, IMutableObject
     {
+        [SerializeField]
+        [Tooltip("Should this mutable object be persisted throughout scene loads")]
+        private bool persisting = false;
+
+        public bool Persisting => persisting;
+
         public abstract void ResetValues();
 
         private void OnValidate()
