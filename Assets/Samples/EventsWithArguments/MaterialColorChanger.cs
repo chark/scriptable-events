536e93204127aa30a52335af43806a74b083194d
Edvinas <edvinas108@gmail.com>
2021-01-24 12:38:10 +0200

    Remove old samples, add samples showing how to use simple & arg events

---
+++ Assets/Samples/EventsWithArguments/MaterialColorChanger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace ScriptableEvents.EventsWithArguments
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    public class MaterialColorChanger : MonoBehaviour
+    {
+        private Material material;
+
+        private void Awake()
+        {
+            material = GetComponent<MeshRenderer>().material;
+        }
+
+        public void ToggleColor()
+        {
+            var color = material.color;
+            material.color = color == Color.black
+                ? Color.white
+                : Color.black;
+        }
+    }
+}
---
+++ Assets/Samples/SimpleEvents/MaterialColorChanger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace ScriptableEvents.SimpleEvents
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    public class MaterialColorChanger : MonoBehaviour
+    {
+        private Material material;
+
+        private void Awake()
+        {
+            material = GetComponent<MeshRenderer>().material;
+        }
+
+        public void ChangeColor(float value)
+        {
+            var color = material.color;
+            color.r = value;
+            color.g = value;
+            color.b = value;
+            material.color = color;
+        }
+    }
+}
