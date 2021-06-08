67bd9806de182b9cb120029179015869784d0635
Edvinas <edvinas108@gmail.com>
2021-06-08 14:08:51 +0300

    Fix samples

--- Packages/com.chark.scriptable-events/Samples~/CustomEvents/Editor/MaterialDataScriptableEventEditor.cs
+++ Packages/com.chark.scriptable-events/Samples~/CustomEvents/Editor/MaterialDataScriptableEventEditor.cs
@@ -5,14 +5,11 @@ using UnityEngine;
 namespace ScriptableEvents.Samples.CustomEvents.Editor
 {
     [CustomEditor(typeof(MaterialDataScriptableEvent))]
-    public class MaterialDataScriptableEventEditor : BaseScriptableEventEditor<MaterialData>
+    public class MaterialDataScriptableEventEditor : TypedScriptableEventEditor<MaterialData>
     {
         protected override MaterialData DrawArgField(MaterialData value)
         {
-            if (value == null)
-            {
-                return new MaterialData(0f, Color.white);
-            }
+            value = new MaterialData(0f, Color.white);
 
             EditorGUILayout.BeginVertical();
             var metallic = EditorGUILayout.Slider("Metallic", value.Metallic, 0f, 1f);
