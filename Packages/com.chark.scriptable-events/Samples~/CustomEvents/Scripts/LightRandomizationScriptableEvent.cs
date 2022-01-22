76ed4db2ccc8ce597aea54e251c05dc95ade68d7
Edvinas <edvinas108@gmail.com>
2022-01-23 01:22:07 +0200

    Update samples, rename default listener editor

---
+++ Packages/com.chark.scriptable-events/Samples~/CustomEvents/Scripts/LightRandomizationScriptableEvent.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace ScriptableEvents.Samples.CustomEvents
+{
+    [CreateAssetMenu(
+        fileName = "LightRandomizationScriptableEvent",
+        menuName = ScriptableEventConstants.MenuNameCustom +
+                   "/Light Randomization Scriptable Event",
+        order = ScriptableEventConstants.MenuOrderCustom + 0
+    )]
+    public class LightRandomizationScriptableEvent
+        : BaseScriptableEvent<LightRandomizationEventArgs>
+    {
+    }
+}
---
+++ Packages/com.chark.scriptable-events/Samples~/CustomEvents/Scripts/LightRandomizationScriptableEventListener.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace ScriptableEvents.Samples.CustomEvents
+{
+    [AddComponentMenu(
+        ScriptableEventConstants.MenuNameCustom + "/Light Randomization Scriptable Event Listener",
+        ScriptableEventConstants.MenuOrderCustom + 0
+    )]
+    public class LightRandomizationScriptableEventListener
+        : BaseScriptableEventListener<LightRandomizationEventArgs>
+    {
+    }
+}
